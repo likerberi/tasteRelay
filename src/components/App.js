0304854b10f0bad1a40d3044e3c7543bc18e10e9
stepbystep <likerberi97@gmail.com>
2019-05-20 16:27:11 +0900

    NavBar with Material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import AppShell from './AppShell';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h2>HELLO</h2>
-      </div>
+      <AppShell />
     )
   }
 }
