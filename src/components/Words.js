caa4732fbc2e8879de08e8be01696b39b9e648c4
stepbystep <likerberi97@gmail.com>
2019-05-22 16:29:49 +0900

    Firebase connected_real_db

--- src/components/Words.js
+++ src/components/Words.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const databaseURL = "https://wordcloud-cc1b5.firebaseio.com";
 
 class Words extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            word: {}
+            //{weight: 0, word: 'noName'}
+        }
+    }
+
+    _get() {
+        fetch(`${databaseURL}/words.json`).then(res => {
+            if(res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(words => this.setState({words: words}))
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextState.words != this.state.words;
+    }
+    // component gen -> 
+    componentDidMount() {
+        this._get();
+    }
+
     render() {
         return (
-            <Card>
-                <CardContent>
-                    samplePage
-                </CardContent>
-            </Card>
-        )
+            <div>
+                {Object.keys(this.state.words).map(id => {
+                    const word = this.state.words[id];
+                    return (
+                        <Card>
+                            <CardContent>
+                                <Typography color="textSecondary" gutterBottom>
+                                    Weight: {word.weight}
+                                </Typography>
+                                <Typography variant="h5" component="h2">
+                                    {word.word}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    );
+                })}
+            </div>
+        );
     }
 }
 
