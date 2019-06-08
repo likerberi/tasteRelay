115698995a5dc835a6485801df5b62b57aa73d3e
stepbystep <likerberi97@gmail.com>
2019-06-08 21:07:37 +0900

    bug fix(s..) and word insertion and delete

--- src/components/Words.js
+++ src/components/Words.js
@@ -2,15 +2,35 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    fab: {
+        position: 'fixed',
+        bottom: '20px',
+        right: '20px'
+    }
+})
 
 const databaseURL = "https://wordcloud-cc1b5.firebaseio.com";
-
 class Words extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            word: {}
+            words: {},
+            dialog: false,
+            word: '',
+            weight: '',
             //{weight: 0, word: 'noName'}
         }
     }
@@ -24,12 +44,70 @@ class Words extends React.Component {
         }).then(words => this.setState({words: words}))
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState.words != this.state.words;
+    _post(word) {
+        return fetch(`${databaseURL}/words.json`, {
+            method: 'POST',
+            body: JSON.stringfy(word)
+        }).then(res => {
+            if(res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(data => {
+            let nextState = this.state.words;
+            nextState[data.name] = word;
+            this.setState({words: nextState});
+        })
     }
+
+    _delete(id) {
+        return fetch(`${databaseURL}/words/${id}.json`, {
+            method: 'DELETE'
+        }).then(res => {
+            if(res.status != 200) {
+                throw new Error(res.statusText);
+            }
+            return res.json();
+        }).then(() => {
+            let nextState = this.state.words;
+            delete nextState[id];
+            this.setState({words: nextState});
+        })
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextState.words != this.state.words;
+    // }
     // component gen -> 
+
+    handleDialogToggle = () => this.setState({
+        dialog: !this.state.dialog
+    })
+
+    handleValueChange = (e) => {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+    }
+
     componentDidMount() {
         this._get();
+    }
+
+    handleSubmit = () => {
+        const word = {
+            word: this.state.word,
+            weight: this.state.weight,
+        }
+        this.handleDialogToggle();
+        if (word.word && !word.weight) {
+            return;
+        }
+        this._post(word);
+    }
+
+    handleDelete = (id) => {
+        this._delete(id);
     }
 
     render() {
@@ -43,9 +121,20 @@ class Words extends React.Component {
                                 <Typography color="textSecondary" gutterBottom>
                                     Weight: {word.weight}
                                 </Typography>
-                                <Typography variant="h5" component="h2">
-                                    {word.word}
-                                </Typography>
+                                <Grid container>
+                                    <Grid item xs={6}>
+                                        <Typography variant="h5" component="h2">
+                                            {word.word}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Button variant="contained" color="primary" onClick={()=> {
+                                            this.handleDelete(id)}}>
+                                            Delete
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                                
                             </CardContent>
                         </Card>
                     );
